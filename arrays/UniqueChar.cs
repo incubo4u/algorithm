addffaf099228f74c690f1ad4e53a9d9a9b195f3
incubo4u <mjk@marxschiller.com>
2021-08-07 15:28:01 +0200

    without additional data structures

---
+++ arrays/UniqueChar.cs
@@ -0,0 +1,31 @@
+﻿using System;
+// implement an algorithm that checks whether a string contains only unique characters
+// without additional data structures
+namespace arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            System.Console.WriteLine(HaveOnlyUniqueChar(str));
+        }
+
+        private static bool HaveOnlyUniqueChar(string str)
+        {
+            char[] charTab = str.ToCharArray();
+            Array.Sort(charTab);
+            int len = charTab.Length;
+            int i = 1;
+            while (i < len)
+            {
+                if (charTab[i - 1] == charTab[i])
+                {
+                    return false;
+                }
+                i++;
+            }
+            return true;
+        }
+    }
+}
