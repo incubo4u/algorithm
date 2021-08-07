211af7468282c0e4948024d75da5d94773441c4f
incubo4u <mjk@marxschiller.com>
2021-08-07 17:00:23 +0200

    check  if ione str is a permutation of the other

---
+++ arrays/StrPermutation.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+namespace arrays
+{
+    static class StrPermutation
+    {
+        public static bool IsPermutationOf(string s, string s1)
+        {
+            if (s.Length != s1.Length) { return false; }
+            int len = s.Length;
+            var dict = new Dictionary<char, int>();
+            for (int i = 0; i < len; i++)
+            {
+                char letter = s[i];
+                if (dict.ContainsKey(letter))
+                {
+                    dict[letter]++;
+                }
+                else
+                {
+                    dict.Add(letter, 1);
+                }
+            }
+            for (int j = 0; j < len; j++)
+            {
+                char letter = s1[j];
+                if (dict.ContainsKey(letter) && dict[letter] > 0)
+                {
+                    dict[letter]--;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
+
+
