063e2330e022e71d088a44a926fdf73c20a86370
incubo4u <mjk@marxschiller.com>
2021-08-26 16:22:02 +0200

    finds the k-th element from the end in a one-way linked list

---
+++ linkedList/FindFromTheEnd.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace linkedList
+{
+    public static class FindFromTheEnd
+    {
+        public static Node FindNode(Node head, int k)
+        {
+            if (k == 1) { return head; }
+            int lenght = 0;
+            var node = head;
+            while (node != null)
+            {
+                lenght++;
+                node = node.next;
+            }
+            node = head;
+            if (k > lenght) { throw new IndexOutOfRangeException(); }
+            for (int i = 1; i < lenght - k; i++)
+            {
+                node = node.next;
+            }
+            return node;
+        }
+    }
+}
