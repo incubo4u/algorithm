d2f2f468a9959f8094905920ff8ded0a722e6a55
incubo4u <mjk@marxschiller.com>
2021-09-21 17:42:07 +0200

    check if there is a cycle and if so, where it starts

--- linkedList/FindCycle.cs
+++ linkedList/FindCycle.cs
@@ -24,12 +24,22 @@ namespace linkedList
             if (head == null || head.next == null) { return null; }
             Node stepOne = head;
             Node stepTwo = head;
+            int steps = 0;
             while (stepOne != null && stepTwo != null)
             {
+                steps++;
                 stepOne = stepOne.next == null ? null : stepOne.next;
                 stepTwo = stepTwo.next == null ||
                           stepTwo.next.next == null ? null : stepTwo.next.next;
-                if (stepOne == stepTwo) { return stepOne.next.next; }
+                if (stepOne == stepTwo) { 
+                    stepOne = head;
+                    for (int i = 0; i < steps; i++)
+                    {
+                        stepOne = stepOne.next;
+                        stepTwo = stepTwo.next;
+                    }
+                    if(stepOne == stepTwo){ return stepOne.next; }
+                }
             }
             return null;
         }
