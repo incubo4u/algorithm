033b22f45c390b552dda5221e984e6a3aa26d71a
incubo4u <mjk@marxschiller.com>
2021-09-23 12:58:42 +0200

    stack implementation

---
+++ stackAndQueues/StackDataStructure.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+
+namespace stackAndQueues
+{
+    public class Stack
+    {
+        Node top;
+        public class Node<T>
+        {
+            T data;
+            Node next = null;
+            public Node(T d)
+            {
+                data = d;
+            }
+        }
+        public T Pop(){
+            if(top != null){
+                T item = top.data;
+                top = top.next;
+                return item;
+            }
+            return null;
+        }
+        public void Push(T item){
+            Node node = new Stack.Node(item);
+            node.next = top;
+            top = node;
+        }
+        public T Peek(){
+            return top.data;
+        }
+    }
+}
