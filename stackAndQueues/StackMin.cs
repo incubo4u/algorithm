f32a7f9d17dc840cf3c3afde0ff3dc0af13990f8
incubo4u <mjk@marxschiller.com>
2021-09-23 15:06:48 +0200

    push() pop() min() in O(1)

---
+++ stackAndQueues/StackMin.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+
+namespace stackAndQueues
+{
+    public class StackMinimal
+    {
+        Node top;
+        Stack minValue = new Stack();
+        public class Node<T>
+        {
+            T data;
+            Node next = null;
+            public Node(T d)
+            {
+                data = d; 
+            }
+        }
+        public T Pop()
+        {
+            if (top != null)
+            {
+                if (top.data == minValue.Peek())
+                {
+                    minValue.Pop();
+                }
+                T item = top.data;
+                top = top.next;
+                return item;
+            }
+            return null;
+        }
+        public void Push(T item)
+        {
+            if (minValue.Peek() <= item)
+            {
+                minValue.Push(item);
+            }
+            Node node = new Node(item);
+            node.next = top;
+            top = node;
+        }
+        public T Peek()
+        {
+            return top.data;
+        }
+        public T Min()
+        {
+            return minValue.Peek();
+        }
+    }
+}
