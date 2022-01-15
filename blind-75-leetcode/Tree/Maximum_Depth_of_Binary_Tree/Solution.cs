0c1d3c84d9885ff9596ddc3d3ff65ce5e8b2562a
mikolaj <incubo4u@mikolajs-MacBook-Pro-2.local>
2022-01-15 11:52:25 +0100

    solution

---
+++ blind-75-leetcode/Tree/Maximum_Depth_of_Binary_Tree/Solution.cs
@@ -0,0 +1,8 @@
+public class Solution
+{
+  public int MaxDepth(TreeNode root)
+  {
+      if(root == null ){ return 0; }
+      return Math.Max((MaxDepth(root.left)), MaxDepth(root.right)) + 1;
+  }
+}
