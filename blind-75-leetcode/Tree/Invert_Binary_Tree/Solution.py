019192f95d7a8b7cfeedfedfbc0e3ee67f323609
incubo4u <mjk@marxschiller.com>
2022-01-24 18:48:35 +0100

    invert binary tree

--- blind-75-leetcode/Tree/Invert_Binary_Tree/Solution.py
+++ blind-75-leetcode/Tree/Invert_Binary_Tree/Solution.py
@@ -1,25 +1,16 @@
 #!/usr/bin/env python
 # Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 from typing import Optional
 
-
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
 class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        if root is None:
-            return None
-        def invertNodes(root: Optional[TreeNode]) -> None:
-            if root is not None:
-                temp = root.left
-                root.left = root.right
-                root.right = temp
-                invertNodes(root.left)
-                invertNodes(root.right)
-            else:
-                return
-        invertNodes(root)        
+        if root is not None:
+            root.left , root.right = root.right , root. left
+            self.invertTree(root.left)
+            self.invertTree(root.right)
         return root
