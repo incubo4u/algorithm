e291c0c68924bace1734b5226440b6cb9739f111
incubo4u <mjk@marxschiller.com>
2022-01-26 00:24:42 +0100

    search in a binary search tree

---
+++ recursion-I-leetcode/Search_in_a_Binary_Search_Tree/Solution.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from typing import Optional
+
+
+class Solution:
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        if root is None:
+            return None
+        if root.val == val:
+            return root
+        left = self.searchBST(root.left,val)
+        if left:
+            return left
+        right = self.searchBST(root.right,val)
+        if right:
+            return right
+                
+            
+        
