fffaa820e5fe5b79998ff538a32ea9bc89cf0528
incubo4u <mjk@marxschiller.com>
2022-02-27 15:28:21 +0100

    combinations

--- recursion-II-leetcode/Combinations/Solution.py
+++ recursion-II-leetcode/Combinations/Solution.py
@@ -1,23 +1,18 @@
-from typing import List 
+from typing import List
+
+
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
         result = []
         sub = []
-        n+=1
-        def comb(i,j):
+        n += 1
+
+        def comb(i):
             if i <= n:
                 if len(sub) < k:
                     sub.append(i)
-                    comb(i+1,j+1)
+                    comb(i+1)
                     sub.pop()
-                    comb(i+1,j+1)
-                else: 
+                    comb(i+1)
+                else:
                     result.append(sub[:])
-            
-                
-        comb(1,0)
-        return result
-s = Solution()
-a = s.combine(4,2)
-for i in range(len(a)):
-    print(a[i])
