1208c1c9af01d6a6174080d8d2bd7385226a2059
incubo4u <mjk@marxschiller.com>
2022-03-13 17:43:31 +0100

    largest rectangle in histogram

--- recursion-II-leetcode/Largest_Rectangle_in_Histogram/Solution.py
+++ recursion-II-leetcode/Largest_Rectangle_in_Histogram/Solution.py
@@ -1,33 +1,33 @@
 
 class Solution(object):
     def largestRectangleArea(self, heights):
-        heights = heights + [0]
+        heights += [0]
         tracking = []
-        mxArea = 0
+        maxArea = 0
         for i in range(len(heights)):
             last = len(heights) + 1
             while len(tracking) > 0 and heights[i] < tracking[-1][0]:
                 last = tracking[-1][1]
-                mxArea = max(mxArea, (i-last)*tracking.pop()[0])
+                maxArea = max(maxArea, (i-last)*tracking.pop()[0])
             if len(tracking) == 0 or heights[i] > tracking[-1][0]:
                 tracking.append((heights[i], min(last, i)))
-        return mxArea
+        return maxArea
 
 
 class Solution(object):
     def largestRectangleArea(self, heights):
-        heights = heights + [0]
+        heights += [0]
         tracking = []
-        mxArea = 0
+        maxArea = 0
 
         def find(i):
-            nonlocal mxArea
+            nonlocal maxArea
             if i == len(heights):
-                return mxArea
+                return maxArea
             last = len(heights) + 1
             while len(tracking) > 0 and heights[i] < tracking[-1][0]:
                 last = tracking[-1][1]
-                mxArea = max(mxArea, (i-last)*tracking.pop()[0])
+                maxArea = max(maxArea, (i-last)*tracking.pop()[0])
             if len(tracking) == 0 or heights[i] > tracking[-1][0]:
                 tracking.append((heights[i], min(last, i)))
             return find(i+1)
