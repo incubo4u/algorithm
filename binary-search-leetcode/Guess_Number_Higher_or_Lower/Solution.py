a1c1e9f9455ed88d4dd7c947e426a22e9d86ec9e
incubo4u <mjk@marxschiller.com>
2022-04-24 22:39:31 +0200

    guess number higher or lower

--- binary-search-leetcode/Guess_Number_Higher_or_Lower/Solution.py
+++ binary-search-leetcode/Guess_Number_Higher_or_Lower/Solution.py
@@ -3,21 +3,9 @@
 # @return -1 if mid is higher than the picked number
 #          1 if mid is lower than the picked number
 #          otherwise return 0
-import time
-
-def guess(mid: int) -> int:
-    picked = 3
-    if mid == picked:
-        return 0
-    if mid > picked:
-        return -1
-    return 1
-
 
 class Solution:
     def guessNumber(self, n) -> int:
-        if n == 2:
-            return 2
         left = 1
         right = n
         mid = (left + right) // 2
