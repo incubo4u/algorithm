f947c43d2d40bb119584b7bc249c6b62a2fb8340
incubo4u <mjk@marxschiller.com>
2022-04-28 11:45:47 +0200

    better solution

--- binary-search-leetcode/Search_for_a_Range/Solution.py
+++ binary-search-leetcode/Search_for_a_Range/Solution.py
@@ -3,28 +3,37 @@ from typing import List
 
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        lenght = len(nums)
-
-        def find():
-            left, right = 0, lenght - 1
+        def find_left(nums, target) -> int:
+            left, right = 0, len(nums) - 1
             while left <= right:
-                if left == right and nums[left] != target:
-                    return -1
+                if left == right and nums[left] == target:
+                    return left
                 mid = (left + right) // 2
-                if nums[mid] == target:
-                    return mid
-                if nums[mid] > target:
+                if nums[mid] < target:
+                    left = mid + 1
+                elif nums[mid] == target:
                     right = mid
                 else:
-                    left = mid + 1
+                    right = mid - 1
             return -1
 
-        start = end = find()
-        if start == -1:
-            return [-1, -1]
-        while start > 0 and nums[start - 1] == target:
-            start -= 1
-        while end < lenght - 1 and nums[end + 1] == target:
-            end += 1
+        def find_right(nums, target) -> int:
+            left, right = 0, len(nums) - 1
+            while left <= right:
+                if left == right or left + 1 == right:
+                    if nums[right] == target:
+                        return right
+                    if nums[left] == target:
+                        return left
+                mid = (left + right) // 2
+                if nums[mid] < target:
+                    left = mid + 1
+                elif nums[mid] == target:
+                    left = mid
+                else:
+                    right = mid - 1
+            return -1
 
-        return [start, end]
+        if len(nums) == 0:
+            return [-1, -1]
+        return [find_left(nums, target), find_right(nums, target)]
