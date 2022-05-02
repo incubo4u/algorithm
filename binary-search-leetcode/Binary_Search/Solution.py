42ca1c3508deaf9d2d200ed67537de0ce0e5869e
incubo4u <mjk@marxschiller.com>
2022-05-02 12:30:59 +0200

    binary search

--- binary-search-leetcode/Binary_Search/Solution.py
+++ binary-search-leetcode/Binary_Search/Solution.py
@@ -1,24 +1,12 @@
-def search(self, nums, target):
-    mid = len(nums) // 2
-    current = nums[mid]
-    if current == target:
-        return mid
-    if mid == 0:
+class Solution:
+    def search(self, nums, target):
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = left + (right - left) // 2
+            if nums[mid] == target:
+                return mid
+            if nums[mid] > target:
+                right = mid - 1
+            else:
+                left = mid + 1
         return -1
-    if current < target:
-        return self.search(nums[mid:], target)
-    if current > target:
-        return self.search(nums[:mid], target)
-
-def search_iter(self, nums, target):
-    left, right = 0, len(nums) - 1
-    while left <= right:
-        mid = left + (right - left) // 2
-        current = nums[mid]
-        if current == target:
-            return mid
-        if current > target:
-            right = mid - 1
-        else:
-            left = mid + 1
-    return -1
