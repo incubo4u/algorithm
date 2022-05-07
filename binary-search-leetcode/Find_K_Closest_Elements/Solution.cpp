232d254ee2ecd2a4c86712f947b8dbcd69b42f53
incubo4u <mjk@marxschiller.com>
2022-05-07 14:13:45 +0200

    find k closest elements

---
+++ binary-search-leetcode/Find_K_Closest_Elements/Solution.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+using std::vector;
+
+vector<int> findClosestElements(vector<int> &arr, int k, int x)
+{
+    int left = 0;
+    int right = arr.size() - k;
+    while (left < right)
+    {
+        int mid = left + (right - left) / 2;
+        if (x - arr[mid] > arr[mid + k] - x)
+        {
+            left = mid + 1;
+        }
+        else
+        {
+            right = mid;
+        }
+    }
+    vector<int> result{arr.begin() + left, arr.begin() + left + k};
+    return result;
+}
