588c856dd95d850aaff0de6c31a653d7f9bda4de
incubo4u <mjk@marxschiller.com>
2022-06-27 13:51:31 +0200

    split array largest sum

---
+++ binary-search-leetcode/Split_Array_Largest_Sum/Solution.py
@@ -0,0 +1,40 @@
+from typing import List
+
+
+class Solution:
+    def splitArray(self, nums: List[int], m: int) -> int:
+        def divideNotExceeding(limit):
+            largestSum = 0
+            currentSum = 0
+            cut = m - 1
+            for elm in nums:
+                if cut < 0:
+                    return -1
+                if currentSum + elm <= limit:
+                    currentSum += elm
+                else:
+                    cut -= 1
+                    currentSum = elm
+                largestSum = max(currentSum, largestSum)
+            if cut > 0:
+                return largestSum
+            elif cut == 0:
+                return 0
+            else:
+                return -1
+
+        left, right = max(nums), sum(nums)
+        while left < right:
+            mid = (left + right) // 2
+            result = divideNotExceeding(mid)
+            if result > 0:
+                right = result
+            elif result == 0:
+                right = mid
+            else:
+                left = mid + 1
+        return left
+
+
+s = Solution()
+print(s.splitArray([7, 2, 5, 10, 8], m=2))
