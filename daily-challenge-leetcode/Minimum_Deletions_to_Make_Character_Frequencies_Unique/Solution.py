acf537ed9bd82b453affb2e0c0eb3ad8134874e6
incubo4u <mjk@marxschiller.com>
2022-06-29 18:44:51 +0200

    minimum deletions to make character frequencies unique

--- daily-challenge-leetcode/Minimum_Deletions_to_Make_Character_Frequencies_Unique/Solution.py
+++ daily-challenge-leetcode/Minimum_Deletions_to_Make_Character_Frequencies_Unique/Solution.py
@@ -3,21 +3,12 @@ from collections import Counter
 
 class Solution:
     def minDeletions(self, s: str) -> int:
-        if len(s) == 1:
-            return
+        frequencies = sorted(Counter(s).values(), reverse=True)
         remove = 0
-        freq = list(Counter(s).values())
-        freq.sort(reverse=True)
-        i, j = 0, 1
-        while j < len(freq):
-            if freq[i] <= 0:
-                break
-            if freq[i] == freq[j]:
-                remove += 1
-                freq[j] -= 1
-                if j + 1 < len(freq):
-                    j += 1
-            else:
-                i += 1
-                j = i + 1
+        freeFreq = len(s)
+        for freq in frequencies:
+            freeFreq = min(freeFreq, freq)
+            remove += freq - freeFreq
+            if freeFreq > 0:
+                freeFreq -= 1
         return remove
