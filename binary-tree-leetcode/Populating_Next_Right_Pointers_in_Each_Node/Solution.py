5355bd1816884f788f310ad71f5a7c7047124855
incubo4u <mjk@marxschiller.com>
2022-07-06 19:41:38 +0200

    populating next right pointers in each node

---
+++ binary-tree-leetcode/Populating_Next_Right_Pointers_in_Each_Node/Solution.py
@@ -0,0 +1,30 @@
+# Definition for a Node.
+from typing import Optional
+
+
+class Node:
+    def __init__(
+        self,
+        val: int = 0,
+        left: "Node" = None,
+        right: "Node" = None,
+        next: "Node" = None,
+    ):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+
+
+class Solution:
+    def connect(self, root: "Optional[Node]") -> "Optional[Node]":
+        if not root:
+            return 
+        if not root.left:
+            return root
+        root.left.next = root.right
+        if root.next:
+            root.right.next = root.next.left
+        self.connect(root.left)
+        self.connect(root.right)
+        return root                 
