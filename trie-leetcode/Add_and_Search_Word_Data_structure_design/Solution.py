978792f838aa71af7709ee1c68cc15d76db71e25
incubo4u <mjk@marxschiller.com>
2022-07-24 22:01:44 +0200

    add and search word data structure

--- trie-leetcode/Add_and_Search_Word_Data_structure_design/Solution.py
+++ trie-leetcode/Add_and_Search_Word_Data_structure_design/Solution.py
@@ -19,8 +19,6 @@ class WordDictionary:
             return self.end
         
         c = word[0]
-        print(word)
-        print(self.childs)
         if c == '.':
             for node in self.childs.values():
                 if node.search(word[1:]):
@@ -29,12 +27,3 @@ class WordDictionary:
         if c in self.childs:
             return self.childs[c].search(word[1:])
         return False
-
-
-# Your WordDictionary object will be instantiated and called as such:
-obj = WordDictionary()
-obj.addWord("dad")
-obj.addWord("mad")
-obj.addWord("bad")
-print(obj.search("pad"))
-print(obj.search("bad"))
