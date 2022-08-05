7dedaf5eeede0a2b71bdb66b9fb91b3be717d61a
incubo4u <mjk@marxschiller.com>
2022-08-05 20:36:18 +0200

    longest substring without reppeating characters

---
+++ hash-table-leetcode/Longest_Substring_Without_Repeating_Characters/Solution.py
@@ -0,0 +1,19 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        seen = {}
+        start = 0
+        result = 0
+        for end, char in enumerate(s):
+            if char not in seen:
+                result = max(result, end - start + 1)
+            else:
+                if seen[char] < start:
+                    result = max(result, end - start + 1)
+                else:
+                    start = seen[char] + 1
+            seen[char] = end
+        return result
+
+
+s = Solution()
+print(s.lengthOfLongestSubstring("dvdf"))
