0361907ffc7808fcc9237f3017e1b756e55a4373
incubo4u <mjk@marxschiller.com>
2022-08-07 23:25:26 +0200

    count vowels permutation

--- daily-challenge-leetcode/Count_Vowels_Permutation/Solution.py
+++ daily-challenge-leetcode/Count_Vowels_Permutation/Solution.py
@@ -1,15 +1,18 @@
 class Solution:
     def countVowelPermutation(self, n: int) -> int:
-        vowel = [[1, 1, 1, 1, 1]]
-        n -= 1
+        a,e,i,o,u=1,1,1,1,1
+        n-=1
         while n > 0:
-            last = vowel[-1]
-            a = last[1] + last[2] + last[4]
-            e = last[0] + last[2]
-            i = last[1] + last[3]
-            o = last[2]
-            u = last[2] + last[3]
-            vowel.append([a, e, i, o, u])
-            n -= 1
-        result = sum(vowel[-1])
-        return result % (pow(10, 9) + 7)
+            n-=1
+            newA = e+i+u
+            newE = a+i
+            newI = a + e + o + u
+            newO = i + u
+            newU = a
+            a = newA
+            e = newE
+            i = newI
+            o = newO
+            u = newU
+        
+        return (a+e+i+o+u) % 1000000007
