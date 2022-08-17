199477bf9eaa75f1bbde731917b62782e4a09ad9
incubo4u <mjk@marxschiller.com>
2022-08-17 10:15:29 +0200

    unique morse code

---
+++ daily-challenge-leetcode/Unique_Morse_Code_Words/Solution.py
@@ -0,0 +1,34 @@
+class Solution:
+    def uniqueMorseRepresentations(self, words: List[str]) -> int:
+        morse = {
+            "a": ".-",
+            "b": "-...",
+            "c": "-.-.",
+            "d": "-..",
+            "e": ".",
+            "f": "..-.",
+            "g": "--.",
+            "h": "....",
+            "i": "..",
+            "j": ".---",
+            "k": "-.-",
+            "l": ".-..",
+            "m": "--",
+            "n": "-.",
+            "o": "---",
+            "p": ".--.",
+            "q": "--.-",
+            "r": ".-.",
+            "s": "...",
+            "t": "-",
+            "u": "..-",
+            "v": "...-",
+            "w": ".--",
+            "x": "-..-",
+            "y": "-.--",
+            "z": "--..",
+        }
+        seen = set()
+        for word in words:
+            seen.add("".join(map(morse.get, word)))
+        return len(seen)
