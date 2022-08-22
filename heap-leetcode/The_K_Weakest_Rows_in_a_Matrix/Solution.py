c35a8e01963d793e7dd5c904efaa0d314762987d
incubo4u <mjk@marxschiller.com>
2022-08-22 19:11:06 +0200

    the k weaknest row in a matrix

--- heap-leetcode/The_K_Weakest_Rows_in_a_Matrix/Solution.py
+++ heap-leetcode/The_K_Weakest_Rows_in_a_Matrix/Solution.py
@@ -4,6 +4,7 @@ from typing import List
 
 class Solution:
     def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
-        mat = [(sum(row), index) for index, row in enumerate(mat)]
-        heapq.heapify(mat)
-        return [smallest[1] for smallest in heapq.nsmallest(k, mat)]
+        heap = []
+        for i, row in enumerate(mat):
+            heapq.heappush(heap, (sum(row), i))
+        return [smallest[1] for smallest in heapq.nsmallest(k, heap)]
