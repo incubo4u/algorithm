64197a797f7aa1f79cb1da7329f2377763d6d157
incubo4u <mjk@marxschiller.com>
2022-08-22 11:42:21 +0200

    power of two

--- daily-challenge-leetcode/Power_of_Two/Solution.py
+++ daily-challenge-leetcode/Power_of_Two/Solution.py
@@ -1,6 +1,4 @@
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
-        if n == 0:
-            return False
-        return n & -(n) == n
+        return n > 0 and n & -(n) == n
         # return n & (n - 1) == 0
