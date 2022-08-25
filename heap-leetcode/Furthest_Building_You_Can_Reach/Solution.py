0d79eae5c729acd7aada1d8ae042e53e088abe14
incubo4u <mjk@marxschiller.com>
2022-08-25 18:41:59 +0200

    furthest building you can reach

---
+++ heap-leetcode/Furthest_Building_You_Can_Reach/Solution.py
@@ -0,0 +1,31 @@
+import heapq
+from typing import List
+
+
+class Solution:
+    def furthestBuilding(self, heights: List[int], bricks: int, ladders: int) -> int:
+        maxheap = []
+        lenght = len(heights)
+        for i in range(1, lenght):
+            if heights[i - 1] >= heights[i]:
+                continue
+            missing = heights[i] - heights[i - 1]
+            if bricks - missing >= 0:
+                bricks -= missing
+                heapq.heappush(maxheap, -missing)
+            elif ladders > 0:
+                lastBiggestMissing = -maxheap[0]
+                if (
+                    maxheap
+                    and bricks + lastBiggestMissing >= missing
+                    and lastBiggestMissing >= missing
+                ):
+                    ladders -= 1
+                    bricks = lastBiggestMissing + bricks - missing
+                    heapq.heappop(maxheap)
+                    heapq.heappush(maxheap, -missing)
+                else:
+                    ladders -= 1
+            else:
+                return i - 1 if i - 1 >= 0 else 0
+        return lenght - 1
