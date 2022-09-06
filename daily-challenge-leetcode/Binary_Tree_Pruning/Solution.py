c6ac291c1c4773691146278c7bfcf567cc6f4628
incubo4u <mjk@marxschiller.com>
2022-09-06 21:37:07 +0200

    binary tree pruning

--- daily-challenge-leetcode/Binary_Tree_Pruning/Solution.py
+++ daily-challenge-leetcode/Binary_Tree_Pruning/Solution.py
@@ -1,9 +1,13 @@
+from typing import Optional
+
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
 class Solution:
     def pruneTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         def traverse(root):
@@ -17,7 +21,8 @@ class Solution:
                 root.right = None
 
             return root.val == 1 or l or r
+
         traverse(root)
         if not root.val and not root.left and not root.right:
-            return 
+            return
         return root
