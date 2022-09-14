cc476babbdd38b1cd9b8465771609b83458f86e1
incubo4u <mjk@marxschiller.com>
2022-09-14 10:31:35 +0200

    pseudo-palindromic paths in a binary tree

--- daily-challenge-leetcode/Pseudo-Palindromic_Paths_in_a_Binary_Tree/Solution.py
+++ daily-challenge-leetcode/Pseudo-Palindromic_Paths_in_a_Binary_Tree/Solution.py
@@ -4,23 +4,19 @@ from typing import Optional
 class Solution:
     def pseudoPalindromicPaths(self, root: Optional[TreeNode]) -> int:
         score = 0
-        
+
         def traverse(root, counter):
-            nonlocal score
             if not root:
                 return
-            counter[root.val]+=1
+
+            counter[root.val] = not counter[root.val]
             if not root.left and not root.right:
-                odd = 0
-                for freq in counter:
-                    if odd > 1: return
-                    if freq == 0: continue
-                    if freq % 2 == 1: odd += 1
-                if odd <= 1:
+                if sum(counter) <= 1:
+                    nonlocal score
                     score += 1
 
             traverse(root.left, counter[::])
             traverse(root.right, counter[::])
 
-        traverse(root, [0 for i in range(10)])
+        traverse(root, [False] * 10)
         return score
