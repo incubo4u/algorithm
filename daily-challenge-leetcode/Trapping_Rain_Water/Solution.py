69df1716581b1d7f084df29b93956d10f10225e7
incubo4u <mjk@marxschiller.com>
2022-09-18 14:15:40 +0200

    trapping rain water

---
+++ daily-challenge-leetcode/Trapping_Rain_Water/Solution.py
@@ -0,0 +1,27 @@
+from typing import List
+
+
+class Solution:
+    def trap(self, H: List[int]) -> int:
+        divide = H.index(max(H))
+        HOne = H[: divide + 1]
+        HTwo = H[divide:][::-1]
+        result = 0
+
+        def get_water(H):
+            while H and H[0] < 1:
+                H.pop(0)
+            if not H:
+                return
+            nonlocal result
+            last_h, last_index = H[0], 0
+            for i, curr_h in enumerate(H):
+                if curr_h < last_h:
+                    continue
+                result += sum(map(lambda h: last_h - h, H[last_index + 1 : i]))
+                last_h, last_index = curr_h, i
+
+        get_water(HOne)
+        get_water(HTwo)
+
+        return result
