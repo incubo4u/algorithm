b487bbe688066490d4f97b9bfb53295c11025d23
incubo4u <mjk@marxschiller.com>
2022-09-19 11:15:03 +0200

    find duplicate file in system

---
+++ daily-challenge-leetcode/Find_Duplicate_File_in_System/Solution.py
@@ -0,0 +1,19 @@
+from collections import defaultdict
+from typing import List
+
+class Solution:
+    def findDuplicate(self, paths: List[str]) -> List[List[str]]:
+
+        content_group = defaultdict(list)
+        for _, directory in enumerate(paths):
+            files = directory.split(" ")
+            path = files.pop(0)
+            if path[-1] != "/":
+                path += "/"
+            for _, (f, content) in enumerate(map(lambda f: f.split("("), files)):
+                content_group[content].append(path + f)
+        ret = []
+        for _, group in enumerate(content_group.values()):
+            if len(group) > 1:
+                ret.append(group)
+        return ret
