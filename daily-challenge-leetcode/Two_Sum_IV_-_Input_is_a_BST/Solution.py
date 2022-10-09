bc07830610894558d7e27e783253a3b8727dbf23
incubo4u <mjk@marxschiller.com>
2022-10-09 12:29:38 +0300

    two sum IV - input is a bst

---
+++ daily-challenge-leetcode/Two_Sum_IV_-_Input_is_a_BST/Solution.py
@@ -0,0 +1,25 @@
+from typing import Optional
+
+# Definition for a binary tree node.
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def findTarget(self, root: Optional[TreeNode], k: int) -> bool:
+        need = set()
+
+        def search(root):
+            if not root:
+                return
+            if root.val in need:
+                return True
+            need.add(k - root.val)
+            return search(root.left) or search(root.right)
+
+        return search(root)
