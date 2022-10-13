f5a165e67d5c74f06b520f4e4d4f87ba405957a1
incubo4u <mjk@marxschiller.com>
2022-10-13 03:34:58 +0300

    delete node in a linked list

--- daily-challenge-leetcode/Delete_Node_in_a_Linked_List/Solution.py
+++ daily-challenge-leetcode/Delete_Node_in_a_Linked_List/Solution.py
@@ -7,8 +7,5 @@
 
 class Solution:
     def deleteNode(self, node):
-        while node.next.next:
-            node.val = node.next.val
-            node = node.next
         node.val = node.next.val
-        node.next = None
+        node.next = node.next.next
