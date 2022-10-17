60a45ccc5eeeccdd961cc9e3fdf7010149d17447
incubo4u <mjk@marxschiller.com>
2022-10-17 18:07:33 +0300

    logger rate limiter

---
+++ hash-table-leetcode/Logger_Rate_Limiter/Solution.py
@@ -0,0 +1,17 @@
+from collections import defaultdict
+
+
+class Logger:
+    def __init__(self):
+        self.log = defaultdict(int)
+
+    def shouldPrintMessage(self, timestamp: int, message: str) -> bool:
+        if self.log[message] <= timestamp:
+            self.log[message] = timestamp + 10
+            return True
+        return False
+
+
+# Your Logger object will be instantiated and called as such:
+# obj = Logger()
+# param_1 = obj.shouldPrintMessage(timestamp,message)
