b3b9b3dfebb5e8ebab7cd4b104ba67a0fbdd8c6b
incubo4u <mjk@marxschiller.com>
2022-10-19 23:49:34 +0300

    n-th tribonacci number

---
+++ dinamic-programming-leetcode/N-th_Tribonacci_Number/Solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        if not n:
+            return 0
+        if n < 3:
+            return 1
+        memo = [0] * (n + 1)
+        memo[1] = 1
+        memo[2] = 1
+        memo[3] = 2
+        for i in range(4, n + 1):
+            memo[i] = memo[i - 1] + memo[i - 2] + memo[i - 3]
+        return memo[n]
