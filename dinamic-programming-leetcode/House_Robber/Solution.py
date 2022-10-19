75c90de4e2a0b6c283c572c98809dce1aad7577b
incubo4u <mjk@marxschiller.com>
2022-10-19 23:39:14 +0300

    house robber

---
+++ dinamic-programming-leetcode/House_Robber/Solution.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) < 2:
+            return nums[0]
+        memo = [0] * len(nums)
+        memo[0] = nums[0]
+        memo[1] = max(nums[1], nums[0])
+        for i in range(2, len(nums)):
+            memo[i] = max(memo[i - 1], memo[i - 2] + nums[i])
+        return memo[-1]
