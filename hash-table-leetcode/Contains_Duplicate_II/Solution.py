51153dad184ca836dc8a73b99bdb4b35e1dcd395
incubo4u <mjk@marxschiller.com>
2022-10-21 12:02:48 +0300

    better solution

--- hash-table-leetcode/Contains_Duplicate_II/Solution.py
+++ hash-table-leetcode/Contains_Duplicate_II/Solution.py
@@ -3,15 +3,11 @@ from typing import List
 
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-        if len(nums) == len(set(nums)):
-            return False
-        h = {}
+        seen = set()
         for i, nr in enumerate(nums):
-            if nr not in h:
-                h[nr] = [i]
-            else:
-                for j, n in enumerate(h[nr]):
-                    if abs(n - i) <= k:
-                        return True
-                h[nr].append(i)
+            if nr in seen:
+                return True
+            seen.add(nums[i])
+            if len(seen) > k:
+                seen.remove(nums[i - k])
         return False
