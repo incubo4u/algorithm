24d05d7346563fdad4f8585e8afe8083b1dc8043
incubo4u <mjk@marxschiller.com>
2022-10-26 15:06:36 +0300

    subarray sum equals k

---
+++ daily-challenge-leetcode/Subarray_Sum_Equals_K/Solution.py
@@ -0,0 +1,19 @@
+from ast import List
+from collections import defaultdict
+
+
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        if len(nums) == 1:
+            return 1 if k == nums[0] else 0
+        d = defaultdict(list)
+        ret, curr = 0, 0
+        for i, nr in enumerate(nums):
+            curr += nr
+            if curr == k:
+                ret += 1
+            if curr - k in d:
+                ret += len(d[curr - k])
+            d[curr].append(i)
+
+        return ret
