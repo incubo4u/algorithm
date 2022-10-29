a6f3d904aeb96457fb4cdc114720826a7304ee7d
incubo4u <mjk@marxschiller.com>
2022-10-29 14:25:02 +0300

    max consecutive ones II

---
+++ daily-challenge-leetcode/Max_Consecutive_Ones_II/Solution.py
@@ -0,0 +1,19 @@
+class Solution:
+    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+        seq = 0
+        best_seq = 0
+        before_zero = 0
+        flip = False
+        for nr in nums:
+            if nr == 1:
+                seq += 1
+                before_zero += 1
+            elif not flip:
+                seq += 1
+                before_zero = 0
+                flip = True
+            else:
+                seq = before_zero + 1
+                before_zero = 0
+            best_seq = max(best_seq, seq)
+        return best_seq
