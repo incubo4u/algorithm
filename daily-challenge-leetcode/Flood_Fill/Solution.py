a6b1ba191a65d70ad2698b1b0673c74317704e39
incubo4u <mjk@marxschiller.com>
2022-10-31 00:44:23 +0200

    flood fill

--- daily-challenge-leetcode/Flood_Fill/Solution.py
+++ daily-challenge-leetcode/Flood_Fill/Solution.py
@@ -6,16 +6,13 @@ class Solution:
         self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
         override = image[sr][sc]
         directions = ((1, 0), (-1, 0), (0, 1), (0, -1))
-        seen = set()
         m, n = len(image), len(image[0])
 
         def dfs(i, j):
             if i > m - 1 or j > n - 1 or 0 > j or 0 > i:
                 return
-            if (i, j) in seen or image[i][j] != override:
+            if image[i][j] == newColor or image[i][j] != override:
                 return
-
-            seen.add((i, j))
             image[i][j] = newColor
             for y, x in directions:
                 dfs(i + y, j + x)
