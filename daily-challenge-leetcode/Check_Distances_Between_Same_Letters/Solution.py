7067db179a8e9ee4f53fb5e6c482e6d1060e07c4
incubo4u <mjk@marxschiller.com>
2022-11-06 22:21:58 +0200

    check distance between same letters

---
+++ daily-challenge-leetcode/Check_Distances_Between_Same_Letters/Solution.py
@@ -0,0 +1,13 @@
+from collections import defaultdict
+from typing import List
+
+
+class Solution:
+    def checkDistances(self, s: str, distance: List[int]) -> bool:
+        seen = defaultdict(int)
+        for i, c in enumerate(s):
+            if c not in seen:
+                seen[c] = i
+            elif (i - seen[c]) - 1 != distance[ord(c) - 97]:
+                return False
+        return True
