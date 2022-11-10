48955ef72837bbc5a62d464627ebd3c469024e1f
incubo4u <mjk@marxschiller.com>
2022-11-10 20:36:47 +0200

    minimum size subarray sum

---
+++ daily-challenge-leetcode/Minimum_Size_Subarray_Sum/Solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        left = 0
+        curr_sum = 0
+        ans = float("inf")
+
+        for right, nr in enumerate(nums):
+            curr_sum += nr
+
+            while curr_sum - nums[left] >= target:
+                curr_sum -= nums[left]
+                left += 1
+            if curr_sum >= target:
+                ans = min(ans, (right - left) + 1)
+        return ans if ans != float("inf") else 0
