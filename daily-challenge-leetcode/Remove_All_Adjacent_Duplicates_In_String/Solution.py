44b65eae6cf84e9bf551d9af420cf819eb05e070
incubo4u <mjk@marxschiller.com>
2022-11-10 15:04:24 +0200

    remove all adjacent duplicates in string

---
+++ daily-challenge-leetcode/Remove_All_Adjacent_Duplicates_In_String/Solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def removeDuplicates(self, s: str) -> str:
+        stack = []
+        for c in s:
+            if not stack:
+                stack.append(c)
+            elif stack[-1] == c:
+                stack.pop()
+            else:
+                stack.append(c)
+        return "".join(stack)
