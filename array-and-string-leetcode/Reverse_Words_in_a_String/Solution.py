f6cc51f6e8519243080f17b0b0f1c7be94a1f50f
incubo4u <mjk@marxschiller.com>
2022-11-12 18:34:41 +0200

    reverse words in a string

---
+++ array-and-string-leetcode/Reverse_Words_in_a_String/Solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        s = s.split()
+        l, r = 0, len(s) - 1
+        while l < r:
+            s[l], s[r] = s[r], s[l]
+            l += 1
+            r -= 1
+        return " ".join(s)
