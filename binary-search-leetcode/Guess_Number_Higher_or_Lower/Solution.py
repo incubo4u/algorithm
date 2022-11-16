0c9e1ef7e02834888d7a9eda16384f50e3163aa8
incubo4u <mjk@marxschiller.com>
2022-11-16 12:50:45 +0200

    🧹

--- binary-search-leetcode/Guess_Number_Higher_or_Lower/Solution.py
+++ binary-search-leetcode/Guess_Number_Higher_or_Lower/Solution.py
@@ -1,23 +1,18 @@
 # The guess API is already defined for you.
-# @param mid, your guess
-# @return -1 if mid is higher than the picked number
-#          1 if mid is lower than the picked number
+# @param num, your guess
+# @return -1 if num is higher than the picked number
+#          1 if num is lower than the picked number
 #          otherwise return 0
-
-
+# def guess(num: int) -> int:
 class Solution:
     def guessNumber(self, n) -> int:
-        left = 1
-        right = n
-        result = guess(mid)
-        while left < right:
-            mid = (left + right) // 2
-            if result == 1:
-                left = mid + 1
+        l, r = 0, n
+        while l <= r:
+            mid = (l + r) // 2
+            g = guess(mid)
+            if g == 0:
+                return mid
+            elif g < 0:
+                r = mid - 1
             else:
-                right = mid
-            result = guess(mid)
-        if result == 0:
-            return mid
-        else:
-            return left - 1
+                l = mid + 1
