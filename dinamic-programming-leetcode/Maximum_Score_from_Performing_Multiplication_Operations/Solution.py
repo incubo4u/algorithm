f2f5a686c59280544cf52224eee105ad7d5d0146
incubo4u <mjk@marxschiller.com>
2022-11-16 20:17:25 +0200

    maximmum score from performing multiplications

---
+++ dinamic-programming-leetcode/Maximum_Score_from_Performing_Multiplication_Operations/Solution.py
@@ -0,0 +1,18 @@
+from functools import cache
+from typing import List
+
+
+class Solution:
+    def maximumScore(self, nums: List[int], multipliers: List[int]) -> int:
+        m, n = len(multipliers), len(nums)
+
+        @cache
+        def get_max(i, left):
+            if i == m:
+                return 0
+            return max(
+                nums[left] * multipliers[i] + get_max(i + 1, left + 1),
+                nums[n - 1 - (i - left)] * multipliers[i] + get_max(i + 1, left),
+            )
+
+        return get_max(0, 0)
