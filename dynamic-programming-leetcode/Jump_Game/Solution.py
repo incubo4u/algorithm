6583b3b84f10a18d69547036f19656ff8a02ac50
incubo4u <mjk@marxschiller.com>
2022-11-18 00:45:51 +0200

    jump game

---
+++ dynamic-programming-leetcode/Jump_Game/Solution.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        last = len(nums) - 1
+        for i in range(len(nums) - 1, -1, -1):
+            if i + nums[i] >= last:
+                last = i
+        return last == 0
