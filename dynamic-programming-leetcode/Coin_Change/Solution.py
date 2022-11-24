2d33621f1ec15b819c2b6808259b5f7119972ba8
incubo4u <mjk@marxschiller.com>
2022-11-24 16:17:44 +0200

    python yapf formatting

--- dynamic-programming-leetcode/Coin_Change/Solution.py
+++ dynamic-programming-leetcode/Coin_Change/Solution.py
@@ -4,7 +4,7 @@ from typing import List
 class Solution:
 
   def coinChange(self, coins: List[int], amount: int) -> int:
-    dp = [amount + 1]      * (amount + 1)
+    dp = [amount + 1]    * (amount + 1)
     dp[0] = 0
     for i in range(1, len(dp)):
       for m in coins:
