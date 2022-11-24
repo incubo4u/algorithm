80202fc97ccf76d3246ea3839cd9871b30c651f5
incubo4u <mjk@marxschiller.com>
2022-11-24 13:09:44 +0200

    integer break

--- daily-challenge-leetcode/Integer_Break/Solution.py
+++ daily-challenge-leetcode/Integer_Break/Solution.py
@@ -1,3 +1,13 @@
-# Integer Break
+class Solution:
+    def integerBreak(self, n: int) -> int:
+        if n <= 3:
+            return n - 1
+        ans = 1
+        while n != 4 and n != 2 and n != 0:
+            n -= 3
+            ans *= 3
+        if n > 0:
+            ans *= n
+        return ans
+# 2 < e < 3
 
-# add yours or the dp one
