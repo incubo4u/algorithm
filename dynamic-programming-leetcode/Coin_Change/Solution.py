f092327aa00c216277abc92278adfc5fd01231b0
incubo4u <mjk@marxschiller.com>
2022-11-24 13:07:34 +0200

    coin change

---
+++ dynamic-programming-leetcode/Coin_Change/Solution.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [amount + 1] * (amount + 1)
+        dp[0] = 0
+        for i in range(1, len(dp)):
+            for m in coins:
+                if i - m >= 0:
+                    dp[i] = min(dp[i - m] + 1, dp[i])
+        return dp[-1] if dp[-1] != amount + 1 else -1
