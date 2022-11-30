6e30f53f400169c06d4ca6882aa0c2fa492f5ad3
incubo4u <mjk@marxschiller.com>
2022-11-30 13:26:12 +0200

    unique number of occurrences

--- daily-challenge-leetcode/Unique_Number_of_Occurrences/Solution.py
+++ daily-challenge-leetcode/Unique_Number_of_Occurrences/Solution.py
@@ -1,8 +1,8 @@
-from ast import List
+from typing import List
 from collections import Counter
 
 
 class Solution:
 
-    def uniqueOccurrences(self, arr: List[int]) -> bool:             # type: ignore
-        return len(set(vals := Counter(arr).values())) == len(vals)  # type: ignore
+    def uniqueOccurrences(self, arr: List[int]) -> bool:             
+        return len(set(vals := Counter(arr).values())) == len(vals)  
