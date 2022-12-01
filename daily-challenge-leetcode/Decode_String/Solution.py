9325ab3b6fc2de8d00442662ec6ca470e27bda9f
incubo4u <mjk@marxschiller.com>
2022-12-01 15:53:03 +0200

    decode string

--- daily-challenge-leetcode/Decode_String/Solution.py
+++ daily-challenge-leetcode/Decode_String/Solution.py
@@ -3,17 +3,17 @@ class Solution:
     def decodeString(self, s):
         n = len(s)
         ans, i = '', 0
-        digit = 0
+        nr = 0
         while i < n:
             c = s[i]
             if c.isalpha():
                 ans += c
             if c.isdigit():
-                digit = digit * 10 + int(c)
+                nr = nr * 10 + int(c)
             elif c == '[':
                 chunck, end = self.decodeString(s[i + 1:])
-                ans += chunck * digit
-                digit = 0
+                ans += chunck * nr
+                nr = 0
                 i += end
             elif c == ']':
                 return ans, i + 1
