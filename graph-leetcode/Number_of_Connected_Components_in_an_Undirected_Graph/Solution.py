6429da9b4a2077906cc77f067ed8e166a6b2f8e9
incubo4u <mjk@marxschiller.com>
2022-12-22 20:15:41 +0200

    bug fix

--- graph-leetcode/Number_of_Connected_Components_in_an_Undirected_Graph/Solution.py
+++ graph-leetcode/Number_of_Connected_Components_in_an_Undirected_Graph/Solution.py
@@ -15,9 +15,9 @@ class UnionFind:
     def union(self, x, y):
         xi, yi = self.find(x), self.find(y)
         if xi != yi:
-            if self.nodes[xi] > self.nodes[yi]:
+            if self.rank[xi] > self.rank[yi]:
                 self.nodes[yi] = xi
-            elif self.nodes[xi] < self.nodes[yi]:
+            elif self.rank[xi] < self.rank[yi]:
                 self.nodes[xi] = yi
             else:
                 self.nodes[xi] = yi
