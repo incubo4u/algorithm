b3d56d4ea10d0e1fbeac489d9526d684deea1803
incubo4u <mjk@marxschiller.com>
2023-01-03 16:12:04 +0200

    delete columns to make sorted

--- daily-challenge-leetcode/Delete_Columns_to_Make_Sorted/Solution.py
+++ daily-challenge-leetcode/Delete_Columns_to_Make_Sorted/Solution.py
@@ -4,13 +4,13 @@ from typing import List
 class Solution:
 
     def minDeletionSize(self, strs: List[str]) -> int:
-        slen = len(strs[0])
+        lenght = len(strs[0])
         ans = 0
-        for i in range(slen):
-            l = ord('a')
+        for i in range(lenght):
+            o = ord('a')
             for s in strs:
-                if l > ord(s[i]):
+                if o > ord(s[i]):
                     ans += 1
                     break
-                l = ord(s[i])
+                o = ord(s[i])
         return ans
