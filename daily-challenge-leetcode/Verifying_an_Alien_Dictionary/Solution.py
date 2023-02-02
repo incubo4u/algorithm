820d653b6814b929bf4c6c387c57c0f4e913133d
incubo4u <mjk@marxschiller.com>
2023-02-02 15:00:40 +0100

    verifying an alien dictionary

---
+++ daily-challenge-leetcode/Verifying_an_Alien_Dictionary/Solution.py
@@ -0,0 +1,20 @@
+class Solution:
+
+    def isAlienSorted(self, words: List[str], order: str) -> bool:
+
+        def cmp(a, b):
+            for i in range(max(len(a), len(b))):
+                if i > len(a) - 1:
+                    return -1
+                elif i > len(b) - 1:
+                    return 1
+                elif abc[a[i]] > abc[b[i]]:
+                    return 1
+                elif abc[a[i]] < abc[b[i]]:
+                    return -1
+            return 0
+
+        abc = {letter: i for i, letter in enumerate(order)}
+        tmp = words[::]
+        words.sort(key=cmp_to_key(cmp))
+        return words == tmp
