d07b0435c1d9aa19ce2a1a7001c1832d8e4dfc97
incubo4u <mjk@marxschiller.com>
2023-02-14 17:58:21 +0100

    flatten a multilevel doubly linked list

---
+++ daily-challenge-leetcode/Flatten_a_Multilevel_Doubly_Linked_List/Solution.py
@@ -0,0 +1,33 @@
+# Definition for a Node.
+from typing import Optional
+
+
+class Node:
+    def __init__(self, val, prev, next, child):
+        self.val = val
+        self.prev = prev
+        self.next = next
+        self.child = child
+
+
+class Solution:
+
+    def flatten(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        if not head:
+            return
+        flat = []
+
+        def inorder(root):
+            if not root:
+                return
+            flat.append(root)
+            inorder(root.child)
+            inorder(root.next)
+
+        inorder(head)
+        for i in range(1, len(flat)):
+            flat[i - 1].next = flat[i]
+            flat[i].prev = flat[i - 1]
+            flat[i].child = None
+        flat[0].child = None
+        return flat[0]
