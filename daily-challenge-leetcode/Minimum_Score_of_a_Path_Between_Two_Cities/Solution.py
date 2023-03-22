9311d3fd81322df21fbbc58f93ecc946aa42c72c
incubo4u <mjk@marxschiller.com>
2023-03-22 23:00:40 +0100

    minimum score of a path between two cities

--- daily-challenge-leetcode/Minimum_Score_of_a_Path_Between_Two_Cities/Solution.py
+++ daily-challenge-leetcode/Minimum_Score_of_a_Path_Between_Two_Cities/Solution.py
@@ -4,22 +4,25 @@ from typing import List
 
 
 class Solution:
+
     def minScore(self, n: int, roads: List[List[int]]) -> int:
         ans = inf
         g = defaultdict(list)
-        for u,v,w in roads:
-            g[u].append((w,v))
-            g[v].append((w,u))
-        
+        for u, v, w in roads:
+            g[u].append((w, v))
+            g[v].append((w, u))
+
         seen = set()
-        def dfs(node,w):
-            nonlocal ans 
-            ans = min(ans,w)
+
+        def dfs(node, w):
+            nonlocal ans
+            ans = min(ans, w)
             if node in seen:
-                return 
+                return
             seen.add(node)
             for distance, city in g[node]:
                 if city != node:
-                    dfs(city,distance)
-        dfs(1,inf)
-        return ans 
+                    dfs(city, distance)
+
+        dfs(1, inf)
+        return ans
