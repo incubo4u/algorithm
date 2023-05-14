74521c9a3e228f98d49ca603fefeec854e786ced
incubo4u <mjk@marxschiller.com>
2023-05-15 00:00:34 +0200

    maximize score after N operations

---
+++ daily-challenge-leetcode/Maximize_Score_After_N_Operations/Solution.py
@@ -0,0 +1,26 @@
+from collections import defaultdict
+from math import gcd
+from typing import List
+
+
+class Solution:
+
+    def maxScore(self, nums: List[int]) -> int:
+        n = len(nums) // 2
+        cache = defaultdict(int)
+
+        def backtrack(times, seen):
+            if seen in cache:
+                return cache[seen]
+            for i in range(len(nums)):
+                if 1 << i & seen:
+                    continue
+                for j in range(i + 1, len(nums)):
+                    if 1 << j & seen:
+                        continue
+                    cache[seen] = max(
+                        cache[seen], (gcd(nums[i], nums[j]) * times) +
+                        backtrack(times + 1, seen | 1 << j | 1 << i))
+            return cache[seen]
+
+        return backtrack(1, 0)
