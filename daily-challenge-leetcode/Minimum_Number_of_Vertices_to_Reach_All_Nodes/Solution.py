46f975d79293d47243951b9072c1d94579c78fdd
Mikolaj <mikolaj.kozakiewicz@swmansion.com>
2023-05-18 21:33:32 +0200

    minimum number of vertices to reach all nodes

--- daily-challenge-leetcode/Minimum_Number_of_Vertices_to_Reach_All_Nodes/Solution.py
+++ daily-challenge-leetcode/Minimum_Number_of_Vertices_to_Reach_All_Nodes/Solution.py
@@ -1,4 +1,4 @@
 class Solution:
 
     def findSmallestSetOfVertices(self, n: int, edges):
-        return set((t[0] for t in edges)) - set((t[1] for t in edges))
+        return {t[0] for t in edges} - {t[1] for t in edges}
