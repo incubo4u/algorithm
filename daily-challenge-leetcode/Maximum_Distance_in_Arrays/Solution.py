1cdc207404af5de0ad597a3975fb0698483e38b5
Mikolaj <mikolaj.kozakiewicz@swmansion.com>
2023-05-18 21:51:07 +0200

    maximum distance in arrays

---
+++ daily-challenge-leetcode/Maximum_Distance_in_Arrays/Solution.py
@@ -0,0 +1,16 @@
+from heapq import heappop, heappush
+from typing import List
+
+
+class Solution:
+
+    def maxDistance(self, arrays: List[List[int]]) -> int:
+        small, big = [], []
+        for i, arr in enumerate(arrays):
+            heappush(small, (arr[0], i))
+            heappush(big, (-arr[-1], i))
+        if small[0][1] == big[0][1]:
+            s, _ = heappop(small)
+            b, _ = heappop(big)
+            return max(-b - small[0][0], -big[0][0] - s)
+        return -big[0][0] - small[0][0]
