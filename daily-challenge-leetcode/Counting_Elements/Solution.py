c9bc8098fe630fe5c5c023d0002a40fa98be160c
Mikolaj <mikolaj.kozakiewicz@swmansion.com>
2023-05-24 01:15:36 +0200

    counting elements

--- daily-challenge-leetcode/Counting_Elements/Solution.py
+++ daily-challenge-leetcode/Counting_Elements/Solution.py
@@ -2,6 +2,7 @@ from collections import Counter
 
 
 class Solution:
+
     def countElements(self, arr: list[int]) -> int:
         arr = Counter(arr)
         return sum(arr[n] for n in arr if n + 1 in arr)
