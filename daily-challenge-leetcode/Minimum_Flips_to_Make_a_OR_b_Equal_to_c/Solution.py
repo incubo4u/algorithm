5ed7a199ba17f55c92a93d8cbc47e52566b172d1
Mikolaj <mikolaj.kozakiewicz@swmansion.com>
2023-06-07 22:33:33 +0200

    minimum flips to make a or b equal to c

---
+++ daily-challenge-leetcode/Minimum_Flips_to_Make_a_OR_b_Equal_to_c/Solution.py
@@ -0,0 +1,5 @@
+class Solution:
+
+    def minFlips(self, a: int, b: int, c: int) -> int:
+        return (bin(((a | b) ^ c) ^ (a & b & ((a | b) ^ c))).count("1") +
+                bin(a & b & ((a | b) ^ c)).count("1") * 2)
