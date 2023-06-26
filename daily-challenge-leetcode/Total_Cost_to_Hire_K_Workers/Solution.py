2cfc6d565a5d2926564762153fbb6c65508184eb
Mikolaj <mikolaj.kozakiewicz@swmansion.com>
2023-06-26 15:54:48 +0200

    total cost to hire k workers

---
+++ daily-challenge-leetcode/Total_Cost_to_Hire_K_Workers/Solution.py
@@ -0,0 +1,32 @@
+from heapq import heapify, heappop, heappush
+
+
+class Solution:
+
+    def totalCost(self, costs: list[int], k: int, cand: int) -> int:
+        l, r = cand - 1, len(costs) - cand
+        ans = 0
+        if cand + cand < len(costs):
+            hql = costs[:l + 1]
+            hqr = costs[r:]
+            heapify(hql)
+            heapify(hqr)
+        else:
+            costs.sort()
+            return sum(costs[:k])
+
+        while (hql or hqr) and k:
+            if hql and (not hqr or hql[0] <= hqr[0]):
+                ans += heappop(hql)
+                if l + 1 < r:
+                    l += 1
+                    heappush(hql, costs[l])
+            elif hqr:
+                ans += heappop(hqr)
+                if l < r - 1:
+                    r -= 1
+                    heappush(hqr, costs[r])
+            else:
+                break
+            k -= 1
+        return ans
