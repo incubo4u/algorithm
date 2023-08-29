b0d91b05720d4c14e47ce175e316da6c28ad956c
Mikolaj <mikolaj.kozakiewicz@swmansion.com>
2023-08-30 01:15:43 +0200

    minimum penality for a shop

--- daily-challenge-leetcode/Minimum_Penalty_for_a_Shop/Solution.py
+++ daily-challenge-leetcode/Minimum_Penalty_for_a_Shop/Solution.py
@@ -2,19 +2,16 @@ class Solution:
 
     def bestClosingTime(self, customers: str) -> int:
         clients_till = []
-        all_y, all_n = 0, 0
         N = len(customers)
-        for j, c in enumerate(customers):
-            all_y += int(c == "Y")
-            all_n += int(c == "N")
-            clients_till.append((all_y, all_n))
-
+        all_y = customers.count('Y')
+        all_n = N - all_y
         min_penality = all_n
-        close_time = 0
-        for j in range(N):
-            pen_close, pen_open = clients_till[j]
-            penality = pen_open + all_n - pen_close
+        curr_y = curr_n = close_time = 0
+        for j, c in enumerate(customers):
+            curr_y += (is_client := int(c == 'Y'))
+            curr_n += int(c == 'N')
+            penality = curr_n + all_n - curr_y
             if penality < min_penality:
                 min_penality = penality
-                close_time = j + int(customers[j] == "Y")
+                close_time = j + is_client
         return close_time
