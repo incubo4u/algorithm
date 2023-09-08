8baac6448f0c6eb86b49ab6aef08598c651d0d15
Mikolaj <mikolaj.kozakiewicz@swmansion.com>
2023-09-08 15:33:07 +0200

    pascal's triangle

--- daily-challenge-leetcode/Pascal's_Triangle/Solution.py
+++ daily-challenge-leetcode/Pascal's_Triangle/Solution.py
@@ -2,21 +2,34 @@ from typing import List
 
 
 class Solution:
+
     def generate(self, numRows: int) -> List[List[int]]:
-        ret = [[1]]
-        def gen(floor,lenght):
-            if lenght > numRows:
-                return
-            nextFloor = []
-            for i in range(lenght):
-                left = floor[i-1] if i-1 >= 0 else 0
-                right = floor[i] if i <= len(floor)-1 else 0
-                sum = left+right
-                nextFloor.append(sum)
-            ret.append(nextFloor)
-            gen(nextFloor,lenght+1)
-        gen([1],2)
-        return ret
+        ans = [[1]]
+        for i in range(1, numRows):
+            n = len(ans[i - 1])
+            ans.append([
+                ans[-1][i - 1] + ans[-1][i] if i and i < n else 1
+                for i in range(n + 1)
+            ])
+        return ans
+
+
+# class Solution:
+#     def generate(self, numRows: int) -> List[List[int]]:
+#         ret = [[1]]
+#         def gen(floor,lenght):
+#             if lenght > numRows:
+#                 return
+#             nextFloor = []
+#             for i in range(lenght):
+#                 left = floor[i-1] if i-1 >= 0 else 0
+#                 right = floor[i] if i <= len(floor)-1 else 0
+#                 sum = left+right
+#                 nextFloor.append(sum)
+#             ret.append(nextFloor)
+#             gen(nextFloor,lenght+1)
+#         gen([1],2)
+#         return ret
 # slower
 # class Solution:
 #     def generate(self, numRows: int) -> List[List[int]]:
