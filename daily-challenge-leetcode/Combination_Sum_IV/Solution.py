deeb84128d3bb9aec752447055b9ff1f324e8dd1
Mikolaj <mikolaj.kozakiewicz@swmansion.com>
2023-09-09 13:28:09 +0200

    combination sum IV

--- daily-challenge-leetcode/Combination_Sum_IV/Solution.py
+++ daily-challenge-leetcode/Combination_Sum_IV/Solution.py
@@ -1,3 +1,4 @@
+from functools import cache
 from typing import List
 
 
@@ -16,10 +17,24 @@ class Solution:
     #     backtrack(target)
     #     return ret
 
+    # def combinationSum4(self, nums: List[int], target: int) -> int:
+    #     cache = {0: 1}
+    #     for total in range(1, target + 1):
+    #         cache[total] = 0
+    #         for n in nums:
+    #             cache[total] += cache.get(total - n, 0)
+    #     return cache[total]
+
     def combinationSum4(self, nums: List[int], target: int) -> int:
-        cache = {0: 1}
-        for total in range(1, target + 1):
-            cache[total] = 0
-            for n in nums:
-                cache[total] += cache.get(total - n, 0)
-        return cache[total]
+
+        @cache
+        def count(t):
+            if t <= 0:
+                return int(not t)
+            ans = 0
+            for i in range(n):
+                ans += count(t - nums[i])
+            return ans
+
+        n = len(nums)
+        return count(target)
