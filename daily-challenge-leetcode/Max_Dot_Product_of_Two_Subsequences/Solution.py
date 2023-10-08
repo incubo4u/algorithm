d56de6d97a83f0ffc1ab371569fc9c5cc21904a2
Mikolaj <mikolaj.kozakiewicz@swmansion.com>
2023-10-08 18:09:11 +0200

    max dot product of two subsequence

--- daily-challenge-leetcode/Max_Dot_Product_of_Two_Subsequences/Solution.py
+++ daily-challenge-leetcode/Max_Dot_Product_of_Two_Subsequences/Solution.py
@@ -4,15 +4,15 @@ class Solution:
         n, m = len(nums1), len(nums2)
         INF = 1001
 
-        @cache
-        def get_max(i, j):
-            if i > n - 1 or j > m - 1:
-                return -INF
-            return max(
-                get_max(i + 1, j + 1) + (use := nums1[i] * nums2[j]),
-                get_max(i + 1, j), get_max(i, j + 1), use)
+        # @cache
+        # def get_max(i, j):
+        #     if i > n - 1 or j > m - 1:
+        #         return -INF
+        #     return max(
+        #         get_max(i + 1, j + 1) + (use := nums1[i] * nums2[j]),
+        #         get_max(i + 1, j), get_max(i, j + 1), use)
 
-        return get_max(0, 0)
+        # return get_max(0, 0)
 
         dp = [[-INF] * (m + 1) for _ in range(n + 1)]
         dp[n][m] = 0
