9d0d3c1d0edaaa8ee2b926e64bf9403d143af412
Mikolaj <mikolaj.kozakiewicz@swmansion.com>
2023-12-12 23:11:00 +0100

    find minimum time to finish all jobs II

---
+++ daily-challenge-leetcode/Find_Minimum_Time_to_Finish_All_Jobs_II/Solution.py
@@ -0,0 +1,10 @@
+from math import ceil
+
+
+class Solution:
+
+    def minimumTime(self, jobs: list[int], workers: list[int]) -> int:
+        days = 0
+        for _, (w, j) in enumerate(zip(sorted(workers), sorted(jobs))):
+            days = max(cesl(j / w), days)
+        return days
