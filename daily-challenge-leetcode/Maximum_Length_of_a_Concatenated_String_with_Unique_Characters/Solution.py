af5be5606501622acb5900e35d89594101656e4c
Mikolaj <mikolaj.kozakiewicz@swmansion.com>
2024-01-23 20:16:00 +0100

    better version from the future :>

--- daily-challenge-leetcode/Maximum_Length_of_a_Concatenated_String_with_Unique_Characters/Solution.py
+++ daily-challenge-leetcode/Maximum_Length_of_a_Concatenated_String_with_Unique_Characters/Solution.py
@@ -1,20 +1,18 @@
-from functools import cache
-
-
 class Solution:
-    def maxLength(self, arr) -> int:
-        lenght = len(arr)
+    def maxLength(self, arr: list[str]) -> int:
+        n = len(arr)
+        def connect(i,seen):
+            if i > n-1:
+                return 0
+            ans = connect(i+1,seen)
+            for c in arr[i]:
+                idx = ord(c) - ord('a')
+                if (seen >> idx ) & 1:
+                    return ans 
+                seen ^= 1 << idx
+            return max(connect(i+1,seen) + len(arr[i]),ans)
+        return connect(0,0)
 
-        @cache
-        def get_max(i, best):
-            if i > lenght - 1:
-                return len(best)
-            curr, scurr = arr[i], set(arr[i])
-            if len(curr) != len(scurr):
-                return get_max(i + 1, best)
-            elif not set(best).intersection(scurr):
-                return max(get_max(i + 1, best + curr), get_max(i + 1, best))
-            else:
-                return get_max(i + 1, best)
+            
+            
 
-        return get_max(0, "")
