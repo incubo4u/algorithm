013d6fa5560ed36cd1f67e95c2ac77386d66fec9
Mikolaj <mikolaj.kozakiewicz@swmansion.com>
2024-03-15 01:28:46 +0100

    binary subarray with sum

--- daily-challenge-leetcode/Binary_Subarrays_With_Sum/Solution.py
+++ daily-challenge-leetcode/Binary_Subarrays_With_Sum/Solution.py
@@ -10,3 +10,15 @@ class Solution:
             ans += freq.get(s - g, 0)
             freq[s] = freq.get(s, 0) + 1
         return ans
+
+    def __numSubarraysWithSum(self, nums: List[int], g: int) -> int:
+        n = len(nums)
+        s = zero = ans = l = 0
+        for r in range(n):
+            s += nums[r]
+            while l < r and (not nums[l] or s > g):
+                zero = int(not nums[l]) * (zero + 1)
+                s -= nums[l]
+                l += 1
+            ans += int(s == g) + (zero * int(s == g))
+        return ans
