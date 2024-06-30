ddc717a5eb8a68ec01b0713ba89ceb17f7b8fb81
incubo4u <mikolaj.kozakiewicz.999@gmail.com>
2024-06-30 23:35:02 +0200

    split bst

--- daily-challenge-leetcode/Split_BST/Solution.py
+++ daily-challenge-leetcode/Split_BST/Solution.py
@@ -1,5 +1,5 @@
-# Runtime Percentile: 40.63750000000002
-# Memory Percentile: 58.56580000000001
+# Runtime Percentile: 89.6415
+# Memory Percentile: 15.537900000000008
 
 
 # Definition for a binary tree node.
@@ -12,16 +12,12 @@ class Solution:
 
     def splitBST(self, root: Optional[TreeNode],
                  target: int) -> List[Optional[TreeNode]]:
+        if not root:
+            return None, None
 
-        def split(root):
-            if not root:
-                return None, None
+        if root.val <= target:
+            root.right, main_tree_node = self.splitBST(root.right, target)
+            return root, main_tree_node
 
-            if root.val <= target:
-                root.right, main_tree_node = split(root.right)
-                return root, main_tree_node
-
-            other_tree_node, root.left = split(root.left)
-            return other_tree_node, root
-
-        return split(root)
+        other_tree_node, root.left = self.splitBST(root.left, target)
+        return other_tree_node, root
